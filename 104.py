a01427fe3687f29aa27ce451d3019f101cd7fd3a
Gustavo dos Anjos <lalodosanjos@gmail.com>
2022-11-28 00:31:56 -0300

    Add ex 103 and 104

---
+++ 104.py
@@ -0,0 +1,16 @@
+def leiaInt(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = str(input(msg))
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('ERRO! Digite um número inteiro válido.')
+        if ok:
+            return valor  # return funciona como se fosse um break
+
+
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar o número {n}.')
