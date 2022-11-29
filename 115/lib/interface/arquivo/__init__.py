efd7195918b841ed06fc922af56c27efc62f3a96
Gustavo dos Anjos <lalodosanjos@gmail.com>
2022-11-29 15:10:00 -0300

    add exercise 115

--- 115/lib/interface/arquivo/__init__.py
+++ 115/lib/interface/arquivo/__init__.py
@@ -1,3 +1,6 @@
+from lib.interface import *
+
+
 def arqExiste(nome):
     try:
         a = open(nome, 'rt')
@@ -6,3 +9,44 @@ def arqExiste(nome):
         return False
     else:
         return True
+
+
+def criarArquivo(nome):
+    try:
+        a = open(nome, 'wt+')
+        a.close()
+    except Exception as erro:
+        print(f'Houve um erro na criação do arquivo: {erro.__cause__}')
+    else:
+        print(f'Arquivo {nome} criado com sucesso.')
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except Exception as erro:
+        print(f'Erro ao ler o arquivo: {erro.__cause__}')
+    else:
+        cabeçalho('PESSOAS CADASTRADAS')
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
+
+
+def cadastrar(arq, nome='Desconhecido', idade=0):
+    try:
+        a = open(arq, 'at')
+    except Exception as erro:
+        print(f'Houve um erro na abertura no arquivo: {erro.__cause__}')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except Exception as erro:
+            print(
+                f'Houve um erro na hora de escrever os dados: {erro.__cause__}')
+        else:
+            print(f'Novo registro de {nome} adicionado.')
+            a.close
--- 115/sistema.py
+++ 115/sistema.py
@@ -2,21 +2,22 @@ from lib.interface import *
 from lib.interface.arquivo import *
 from time import sleep
 
-arq = 'pessoas.txt'
+arq = '115\pessoas.txt'
 
-if arqExiste(arq):
-    print('Arquivo encontrado com sucesso!')
-else:
-    print('Arquivo não encontrado.')
+if not arqExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resposta = menu(
         ['Ver pessoa cadastrada', 'Cadastrar nova pessoa', 'Sair do sistema'])
     sleep(0.3)
     if resposta == 1:
-        cabeçalho('Opção 1')
+        lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        cabeçalho('NOVO CADASTRO')
+        nome = input('Nome: ')
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabeçalho('Você saiu do sistema.')
         break
